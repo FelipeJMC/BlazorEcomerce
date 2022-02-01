5fb68b2a96bcc25807f6ec0a9403ee1bcfa83acd
Felipe Morales <felipe.moralescandia@gmail.com>
2022-02-01 18:23:41 -0300

    Primer commit
    Signed-off-by: Felipe Morales <felipe.moralescandia@gmail.com>

---
+++ BlazorEcomerce/Shared/Producto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcomerce.Shared
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string ImagenUrl { get; set; } = string.Empty;
+        public decimal Precio { get; set; }
+
+    }
+}
